d93e325ca662c457345c70030067d16e530e3415
smoealy <129239220+smoealy@users.noreply.github.com>
2025-05-03 15:15:08 -0400

    Update Donate.js

--- components/Donate.js
+++ components/Donate.js
@@ -1,1 +1,10 @@
+import React from 'react';
+import { View, Text } from 'react-native';
 
+export default function Donate() {
+  return (
+    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+      <Text>Donate to Sadaqah Fund</Text>
+    </View>
+  );
+}
