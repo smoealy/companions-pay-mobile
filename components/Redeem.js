6e14cf1f9e1ffbb5fa68cfda4bcb60411df4a800
smoealy <129239220+smoealy@users.noreply.github.com>
2025-05-03 15:15:27 -0400

    Update Redeem.js

--- components/Redeem.js
+++ components/Redeem.js
@@ -1,1 +1,10 @@
+import React from 'react';
+import { View, Text } from 'react-native';
 
+export default function Redeem() {
+  return (
+    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+      <Text>Redeem IHRAM for Umrah packages</Text>
+    </View>
+  );
+}
