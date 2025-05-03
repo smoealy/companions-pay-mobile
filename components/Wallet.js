b8ee1948e4016f8f692e5decf58ced7a92d4a900
smoealy <129239220+smoealy@users.noreply.github.com>
2025-05-03 15:14:33 -0400

    Update Wallet.js

--- components/Wallet.js
+++ components/Wallet.js
@@ -1,1 +1,10 @@
+import React from 'react';
+import { View, Text } from 'react-native';
 
+export default function Wallet() {
+  return (
+    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+      <Text>Wallet Balance: Coming Soon</Text>
+    </View>
+  );
+}
