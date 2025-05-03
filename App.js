3a561606000b4758181b43bc5123ef926807e343
smoealy <129239220+smoealy@users.noreply.github.com>
2025-05-03 15:11:46 -0400

    Update App.js

--- App.js
+++ App.js
@@ -1,1 +1,11 @@
+import React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import Tabs from './navigation/Tabs';
 
+export default function App() {
+  return (
+    <NavigationContainer>
+      <Tabs />
+    </NavigationContainer>
+  );
+}
