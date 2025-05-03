1acc85fc37cb6d4ed0bccb3dc699541fad23ba3b
smoealy <129239220+smoealy@users.noreply.github.com>
2025-05-03 15:14:49 -0400

    Update Stake.js

--- components/Stake.js
+++ components/Stake.js
@@ -1,1 +1,10 @@
+import React from 'react';
+import { View, Text } from 'react-native';
 
+export default function Stake() {
+  return (
+    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+      <Text>Stake your IHRAM tokens here</Text>
+    </View>
+  );
+}
